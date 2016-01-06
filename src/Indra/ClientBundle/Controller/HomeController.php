79d1ca4cd98998ebbb74d38c2edbd3ba6326d7f4
fabienwandji <fabienwandji@gmail.com>
2016-01-06 23:32:31 +0100

    mise en place des partenaires de l'hotel

--- src/Indra/ClientBundle/Controller/HomeController.php
+++ src/Indra/ClientBundle/Controller/HomeController.php
@@ -8,11 +8,16 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        $em         = $this->getDoctrine()->getManager();
-        $entities   = $em->getRepository('IndraAdminBundle:CategorieChambre')->findAll();
+        $em          = $this->getDoctrine()->getManager();
+        $entities    = $em->getRepository('IndraAdminBundle:CategorieChambre')->findAll();
+        $partenaires = $em->getRepository('IndraAdminBundle:Gallery')->findBy(
+            array('nom' => 'partenaires')
+        );
+
 
         return $this->render('IndraClientBundle:Home:index.html.twig', array(
-            'entities' => $entities,
+            'entities'    => $entities,
+            'partenaires' => $partenaires[0]
         ));
     }
 }
